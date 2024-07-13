7e7f34628e90528bd16146025331898a88f37e1d
TanganHitam1 <blackjackm2709@gmail.com>
2024-07-13 05:34:15 +0000

    chore: Add /api/whoami endpoint for retrieving user information

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', function (req, res){
+  res.json({ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']})
+})
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
